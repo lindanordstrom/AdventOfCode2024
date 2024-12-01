70ab49472e2ef464b3ac8bb4d57edc0f2fe25934
Linda <lindanordstrom86@gmail.com>
2024-12-01 12:41:01 +0100

    Initial commit

---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode2024;
+
+internal static class Program
+{
+    private static string[] TestInput => 
+        """
+        
+        """
+            .Split(" ");
+
+    private static string[] _realInput = InputLoader.Load(1, separator: " ");
+
+    private static void Main(string[] args)
+    {
+        Day1.Part1(TestInput);
+        Day1.Part2(TestInput);
+    }
+}
